3d1bedc33b2d8a2dc0ba265aadfb4c1b90258663
Mohan Lal Sharma <mohan202145@gmail.com>
2023-09-23 15:01:46 +0530

    Adds Articles template

---
+++ config/Migrations/20230923072525_Initial.php
@@ -0,0 +1,55 @@
+<?php
+
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration {
+    public function up() {
+
+        $this->table('articles')
+
+            ->addColumn('title', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 191,
+                'null' => false,
+            ])
+            ->addColumn('body', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('published', 'boolean', [
+                'default' => false,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addIndex(
+                [
+                    'slug',
+                ],
+                ['unique' => true]
+            )
+
+            ->create();
+    }
+
+    public function down() {
+
+
+        $this->table('articles')->drop()->save();
+    }
+}
